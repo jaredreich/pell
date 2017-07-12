7306eac9d0d36c872e37776b0488bcf01e855bec
Jared Reich <jaredreich@gmail.com>
2017-07-12 12:58:52 -0600

    Alphabetize and clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,57 @@
-const rollup = require('gulp-rollup')
 const babel = require('rollup-plugin-babel')
-const uglify = require('rollup-plugin-uglify')
-const gulp = require('gulp')
 const cssnano = require('gulp-cssnano')
+const del = require('del')
+const gulp = require('gulp')
 const rename = require('gulp-rename')
+const Rollup = require('rollup')
+const rollup = require('gulp-rollup')
+const run = require('run-sequence')
 const sass = require('gulp-sass')
 const size = require('gulp-size')
-const del = require('del')
-const run = require('run-sequence')
+const uglify = require('rollup-plugin-uglify')
 
 gulp.task('clean', () => del(['./dist']))
 
 const rollupConfig = minimize => ({
-  rollup: require('rollup'),
+  rollup: Rollup,
   entry: './src/pell.js',
   moduleName: 'pell',
   format: 'umd',
   exports: 'named',
-  plugins: [
-    babel({
-      exclude: 'node_modules/**'
-    })
-  ].concat(minimize
-    ? [
-      uglify({
-        compress: { warnings: false },
-        mangle: true,
-        sourceMap: false
-      })
-    ]
-    : []
+  plugins: [babel({ exclude: 'node_modules/**' })].concat(
+    minimize
+      ? [
+        uglify({
+          compress: { warnings: false },
+          mangle: true,
+          sourceMap: false
+        })
+      ]
+      : []
   )
 })
 
 gulp.task('script', () => {
   gulp.src('./src/*.js')
-  .pipe(rollup(rollupConfig(false)))
-  .pipe(size({showFiles: true}))
-  .pipe(gulp.dest('./dist'))
+    .pipe(rollup(rollupConfig(false)))
+    .pipe(size({ showFiles: true }))
+    .pipe(gulp.dest('./dist'))
+
   gulp.src('./src/*.js')
-  .pipe(rollup(rollupConfig(true)))
-  .pipe(rename('pell.min.js'))
-  .pipe(size({showFiles: true}))
-  .pipe(size({gzip: true, showFiles: true}))
-  .pipe(gulp.dest('./dist'))
+    .pipe(rollup(rollupConfig(true)))
+    .pipe(rename('pell.min.js'))
+    .pipe(size({ showFiles: true }))
+    .pipe(size({ gzip: true, showFiles: true }))
+    .pipe(gulp.dest('./dist'))
 })
 
 gulp.task('style', () => {
   gulp.src(['./src/pell.scss'])
-  .pipe(sass().on('error', sass.logError))
-  .pipe(size({showFiles: true}))
-  .pipe(gulp.dest('./dist'))
-  .pipe(cssnano())
-  .pipe(rename('pell.min.css'))
-  .pipe(size({showFiles: true}))
-  .pipe(gulp.dest('./dist'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist'))
+    .pipe(cssnano())
+    .pipe(rename('pell.min.css'))
+    .pipe(gulp.dest('./dist'))
 })
 
 gulp.task('default', ['clean'], () => {
