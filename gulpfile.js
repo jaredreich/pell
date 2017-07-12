4dd1774d8c9d1faec3a27c66dae7e5a32419f88e
SidKwok <oceankwok@hotmail.com>
2017-07-12 20:53:51 +0800

    add gulp-size

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const gulp = require('gulp')
 const cssnano = require('gulp-cssnano')
 const rename = require('gulp-rename')
 const sass = require('gulp-sass')
+const size = require('gulp-size')
 const del = require('del')
 const run = require('run-sequence')
 
@@ -35,19 +36,24 @@ const rollupConfig = minimize => ({
 gulp.task('script', () => {
   gulp.src('./src/*.js')
   .pipe(rollup(rollupConfig(false)))
+  .pipe(size({showFiles: true}))
   .pipe(gulp.dest('./dist'))
   gulp.src('./src/*.js')
   .pipe(rollup(rollupConfig(true)))
   .pipe(rename('pell.min.js'))
+  .pipe(size({showFiles: true}))
+  .pipe(size({gzip: true, showFiles: true}))
   .pipe(gulp.dest('./dist'))
 })
 
 gulp.task('style', () => {
   gulp.src(['./src/pell.scss'])
   .pipe(sass().on('error', sass.logError))
+  .pipe(size({showFiles: true}))
   .pipe(gulp.dest('./dist'))
   .pipe(cssnano())
   .pipe(rename('pell.min.css'))
+  .pipe(size({showFiles: true}))
   .pipe(gulp.dest('./dist'))
 })
 
