33b9bbdc7463d76ef7582a734ba3bd071560cdf5
Deforder <peeratant@gmail.com>
2017-07-13 21:28:03 +0700

    Resolve Tab bug and formatBlock bug

--- src/pell.js
+++ src/pell.js
@@ -124,6 +124,27 @@ export const init = settings => {
   const editor = document.createElement('div')
   editor.contentEditable = true
   editor.className = settings.classes.editor
+  editor.enterKeyState = false
+  let enterKeyState = editor.enterKeyState
+  editor.onkeydown = event => {
+    let insertElement
+    if (event.which === 13) {
+      if (enterKeyState) {
+        insertElement = '<br><br>'
+        execute('outdent')
+      }
+      enterKeyState = true
+    } else {
+      if (event.which === 9) {
+        insertElement = '&#09'
+      }
+      enterKeyState = false
+    }
+    if (insertElement) {
+      event.preventDefault()
+      execute('insertHTML', insertElement)
+    }
+  }
   editor.oninput = event => settings.onChange && settings.onChange(event.target.innerHTML)
   root.appendChild(editor)
 
