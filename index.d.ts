6fc60a81b327fb6e35c1c85fb4e150f6fdf28ff2
Cameron <cameron@biteable.com>
2018-03-15 23:41:23 +1100

    added missing settings to interface

--- index.d.ts
+++ index.d.ts
@@ -6,18 +6,30 @@ export function exec(command: string, value?: string | null): boolean
 export function init(settings: Settings): HTMLElement
 
 export interface Action {
-  icon: string
-  title: string
-  state: () => void
+  name: string
+  icon?: string
+  title?: string
   result: () => void
 }
 
-export interface ActionTree {
-  [key: string]: Action
-}
-
 export interface Settings {
+  // The element to render to
   element: HTMLElement
+
+  // Use the output html, triggered by element's `oninput` event
+  onChange: (html: string) => void
+
+  // Instructs the editor which element to inject via the return key
   defaultParagraphSeperator?: string
-  actions?: ActionTree
+
+  // Outputs <span style="font-weight: bold;"></span> instead of <b></b>
+  styleWithCSS?: boolean
+
+  // Specify the actions you specifically want (in order)
+  actions?: Array<Action | string>
+
+  // Choose your custom class names
+  classes?: {
+    [key: 'actionbar' | 'button' | 'content' | 'selected']: string
+  }
 }
